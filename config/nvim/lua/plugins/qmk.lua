e1488d106aa70fac7b21c5635e64311e76353c3d
Carsten <modebm@gmail.com>
2024-02-18 12:17:10 +0100

    override comments for de_ansi layout

--- config/nvim/lua/plugins/qmk.lua
+++ config/nvim/lua/plugins/qmk.lua
@@ -8,6 +8,20 @@ return {
     qmk.setup({
       name = "corne",
       variant = "zmk",
+      comment_preview = {
+        keymap_overrides = {
+          Y = "Z",
+          Z = "Y",
+          MINUS = "ß",
+          EQUAL = "´",
+          LBKT = "Ü",
+          RBKT = "+",
+          SEMI = "Ö",
+          SQT = "Ä",
+          BSLH = "#",
+          FSLH = "-",
+        },
+      },
       layout = {
         "x x x x x x _ _ _ x x x x x x",
         "x x x x x x _ _ _ x x x x x x",
