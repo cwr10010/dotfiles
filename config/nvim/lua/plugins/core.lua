6c0d670051c9869ffb243ad12ff148cada0ee5aa
Carsten <modebm@gmail.com>
2024-02-18 09:42:24 +0100

    extract noice and notify into their own files in nvim

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -1,20 +1,6 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe",
-    },
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 10000,
-    },
-  },
-  {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      opts.presets.lsp_doc_border = true
-    end,
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "catppuccin-frappe",
   },
 }
