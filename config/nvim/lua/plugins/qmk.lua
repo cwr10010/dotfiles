500fc547542391d6ed5ace0872d9511bd0feb1bd
Carsten <modebm@gmail.com>
2024-02-24 10:09:52 +0100

    some minor changes

--- config/nvim/lua/plugins/qmk.lua
+++ config/nvim/lua/plugins/qmk.lua
@@ -3,25 +3,12 @@ return {
   dependencies = {
     "joelspadin/tree-sitter-devicetree",
   },
+  event = "VeryLazy",
   config = function()
     local qmk = require("qmk")
     qmk.setup({
       name = "corne",
       variant = "zmk",
-      comment_preview = {
-        keymap_overrides = {
-          Y = "Z",
-          Z = "Y",
-          MINUS = "ß",
-          EQUAL = "´",
-          LBKT = "Ü",
-          RBKT = "+",
-          SEMI = "Ö",
-          SQT = "Ä",
-          BSLH = "#",
-          FSLH = "-",
-        },
-      },
       layout = {
         "x x x x x x _ _ _ x x x x x x",
         "x x x x x x _ _ _ x x x x x x",
