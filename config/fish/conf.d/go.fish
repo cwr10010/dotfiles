2f6e156b3ffc5594ed16b46d938998a88e082a60
Carsten Rose <carsten.rose@epost-dev.de>
2024-04-19 13:09:18 +0200

    fix go config for fish

--- config/fish/conf.d/go.fish
+++ config/fish/conf.d/go.fish
@@ -1,6 +1,6 @@
 # setup golang here
 set -x GOPATH "$HOME/.go"
-set -x GOROOT (brew --prefix golang)"/libexec"
+set -x GOROOT (/opt/homebrew/bin/brew --prefix golang)"/libexec"
 set -x PATH $GOPATH/bin $GOROOT/bin $PATH
 test -d "$GOPATH" || mkdir "$GOPATH"
 test -d "$GOPATH/src/github.com" || mkdir -p "$GOPATH/src/github.com"
