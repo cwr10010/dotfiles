3c293116de610b52422455777922559985186aed
Carsten <modebm@gmail.com>
2025-02-18 21:02:52 +0100

    some minor config for blink.cmp

--- config/nvim/lua/plugins/blink-cmp.lua
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -12,17 +12,42 @@ return {
       "rafamadriz/friendly-snippets",
     },
     opts = {
+      completion = {
+        trigger = {
+          show_on_insert_on_trigger_character = true,
+          show_on_x_blocked_trigger_characters = { "'", '"', "(", "{", "[" },
+        },
+        ghost_text = {
+          enabled = true,
+        },
+      },
+
       keymap = {
         preset = "default",
       },
 
       appearance = {
         use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
       },
 
       signature = {
         enabled = true,
       },
+
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        providers = {
+          path = {
+            opts = {
+              get_cwd = function(_)
+                return vim.fn.getcwd()
+              end,
+            },
+          },
+        },
+      },
     },
+    opts_extend = { "sources.default" },
   },
 }
