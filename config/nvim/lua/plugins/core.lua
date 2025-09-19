a9e7fa6c251928efeff437ae7df38f6f88262d28
Carsten <modebm@gmail.com>
2025-09-19 08:56:49 +0200

    remove qick fix to get_theme crash

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -5,14 +5,4 @@ return {
       colorscheme = "catppuccin-frappe",
     },
   },
-  {
-    "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
-  },
 }
